5643349acc534cb1203e8c2a2af9d832805db38a
镱见飞行 <150343565+zyicome@users.noreply.github.com>
2024-03-01 19:28:23 +0800

    Delete install directory

--- install/my_msgss/share/my_msgss/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(my_msgss_IDL_FILES "msg/Yolopoint.idl;msg/Yolopoints.idl;msg/Distpoint.idl;msg/Distpoints.idl;msg/Point.idl;msg/Points.idl")
-set(my_msgss_INTERFACE_FILES "msg/Yolopoint.msg;msg/Yolopoints.msg;msg/Distpoint.msg;msg/Distpoints.msg;msg/Point.msg;msg/Points.msg")
