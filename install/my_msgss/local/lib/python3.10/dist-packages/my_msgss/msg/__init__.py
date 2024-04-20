e5dd7933ec9deedb255fdfc4063462919a542fdb
镱见飞行 <150343565+zyicome@users.noreply.github.com>
2024-04-20 13:54:01 +0800

    Delete install directory

--- install/my_msgss/local/lib/python3.10/dist-packages/my_msgss/msg/__init__.py
+++
@@ -1,6 +0,0 @@
-from my_msgss.msg._distpoint import Distpoint  # noqa: F401
-from my_msgss.msg._distpoints import Distpoints  # noqa: F401
-from my_msgss.msg._point import Point  # noqa: F401
-from my_msgss.msg._points import Points  # noqa: F401
-from my_msgss.msg._yolopoint import Yolopoint  # noqa: F401
-from my_msgss.msg._yolopoints import Yolopoints  # noqa: F401
